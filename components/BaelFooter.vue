d0526718edc0ae62f23491e8d9f4e6e8848bd2c9
jjllmm <43472169+jjllmm@users.noreply.github.com>
2021-02-07 14:35:08 +0100

    Update BaelFooter.vue

--- components/BaelFooter.vue
+++ components/BaelFooter.vue
@@ -32,11 +32,10 @@
           </ul>
         </div>
       </div>
-      <div
-        class="xs-text-left xs-p2 xs-border xs-border-right-none xs-border-bottom-none"
+      <div class="xs-text-left xs-p2 xs-border xs-border-right-none xs-border-bottom-none"
         :class="signupAboutSize"
       >
-        <div class="item">
+        <!--div class="item">
           <div class="footer__heading xs-mb2">Deploy</div>
 
           <a
@@ -48,13 +47,12 @@
               title="Deploy to Netlify"
             />
           </a>
-        </div>
+        </div-->
       </div>
       <div class="c-12 xs-text-left xs-p2 xs-border xs-border-right-none xs-border-bottom-none">
         <div class="item xs-text-6">
-          <a href="https://github.com/jake-101/bael-template">Bael</a>
-          <span class="small-txt" v-if="version">{{version}}</span>, An open source design by
-          <a href="https://jake101.com">jake101</a>
+          <a href="#">Hermine</a>
+           © 2021
         </div>
       </div>
     </div>
