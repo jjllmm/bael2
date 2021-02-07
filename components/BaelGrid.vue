879975277ddc0a1e512afff0b228617ee5084f73
jjllmm <43472169+jjllmm@users.noreply.github.com>
2021-02-07 14:00:57 +0100

    Update BaelGrid.vue

--- components/BaelGrid.vue
+++ components/BaelGrid.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="['xs-text-6 md-text-5 bael-grid', {paginated: hasPagination}]">
 <div class="r no-gap">
-        <div class="item xs-text-6"><a href="https://github.com/jake-101/bael-template">Bael</a> <span class="small-txt">1.0 dev</span>, An open source design by
+        <div class="item sm-border-bottom xs-text-6"><a href="https://github.com/jake-101/bael-template">Bael</a> <span class="small-txt">1.0 dev</span>, An open source design by
         <a href="https://jake101.com">jake101</a>
         </div>
 </div>
