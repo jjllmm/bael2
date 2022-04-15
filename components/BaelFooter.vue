e50e8abe298ef3f8d5a86a7665be38a8c87ff4f5
jjllmm <43472169+jjllmm@users.noreply.github.com>
2022-04-15 17:10:12 +0200

    Update BaelFooter.vue

--- components/BaelFooter.vue
+++ components/BaelFooter.vue
@@ -105,7 +105,7 @@ footer .item:hover .footer__heading {
 .footer__heading {
   max-width: 100%;
   pointer-events: none;
-  color: rgba(119, 119, 119, 0.4);
+  color: #aaaaac;
   font-weight: 800;
   font-size: 85%;
   transition: 0.1s color ease-in-out;
