b32576684637e21f70b2af6fdb3301b00a9a0734
jjllmm <43472169+jjllmm@users.noreply.github.com>
2023-01-03 21:43:18 +0100

    Update FullGrid.vue

--- components/FullGrid.vue
+++ components/FullGrid.vue
@@ -140,6 +140,49 @@ export default {
 };
 </script>
 
+<script>
+$(document).ready(function(){
+  
+  function freshDot(){
+    this.obj = document.createElement("div");
+    this.obj.classList.add("box");
+    this.obj.style.top = (window.innerHeight * Math.random()) + 'px';
+    this.obj.style.left = (window.innerWidth * Math.random()) + 'px';
+    this.size = Math.floor(5 * Math.random()) + 7;
+    this.obj.style.height =  this.size + 'px';
+    this.obj.style.width = this.size + 'px';
+    
+    document.body.appendChild(this.obj);
+  }
+  var dot = [];
+  for(var i = 0 ; i < 200; i++ ){
+    dot.push(new freshDot());
+  }
+  /*
+  $(window).resize(function(){
+    for(i=0;i<200;i++){
+      document.body.removeChild(dot[i]);
+    }
+  });
+  */
+});
+</script>
+
+<style>
+body
+  background-color: #ddd
+  height: 100vh
+  width: 100%
+  position: relative
+  
+  
+.box
+  background-color: #000
+  position: fixed
+  border-radius: 50%
+
+</style>
+
 
 <style>
 img[lazy="loading"] {
