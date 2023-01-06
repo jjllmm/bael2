c1f1812a0cb7f51bb47629e769ca50ae84efa1d5
jjllmm <43472169+jjllmm@users.noreply.github.com>
2023-01-06 18:36:35 +0100

    Update createDots.js

--- static/js/createDots.js
+++ static/js/createDots.js
@@ -1,7 +1,7 @@
 function createDot(x, y) {
     var elem=document.createElement("div");
     elem.setAttribute("class", "dot");
-    elem.setAttribute("style", "left:"+x+"px;top:"+y+"200px;");
+    elem.setAttribute("style", "left:"+x+"200px;top:"+y+"200px;");
     document.getElementsByTagName("body")[0].appendChild(elem);
     return elem;
 }
