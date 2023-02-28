e9d8e9d54fd4b1d8abfc949853ee85b27de886e8
jjllmm <43472169+jjllmm@users.noreply.github.com>
2023-02-28 15:53:15 +0100

    Update BaelHeader.vue

--- components/BaelHeader.vue
+++ components/BaelHeader.vue
@@ -3,7 +3,19 @@
     <div class="r xs-border-bottom">
       <div class="c-4 xs-text-left xs-p2 sm-border-bottom-none sm-border-top-none sm-border-left-none border-right border-top">
         <div class="item">
-          <!--nuxt-link class="sitename" to="/" exact>{{$store.state.info.sitename}}</nuxt-link-->
+          <!--nuxt-link to="/" exact>{{$store.state.info.sitename}}</nuxt-link-->
+          <nuxt-link
+          
+          to="/"
+          exact
+        >
+          <img
+            style="width:180px;"
+            class="xs-block xs-fit xs-mb2"
+            :src="info.siteicon"
+            :alt="info.sitename"
+          />
+        </nuxt-link>
           
         </div>
       </div>
